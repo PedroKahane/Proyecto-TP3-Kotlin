a0bc8080facd8a8ba3c5d79b373e57a3177019a9
Santiago Varela <santiago.varela@hotmai>
2023-11-01 12:04:13 -0300

    Inicio del Login

---
+++ app/src/main/java/com/example/proyecto_tp3_kotlin/login_activity/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.proyecto_tp3_kotlin.login_activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.proyecto_tp3_kotlin.MainActivity
+import com.example.proyecto_tp3_kotlin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        var buttonLogin = findViewById<Button>(R.id.button_login)
+
+        buttonLogin.setOnClickListener{
+
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent)
+
+        }
+
+    }
+}
