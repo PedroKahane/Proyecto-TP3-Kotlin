f3d027151de873fdb4d1a846704d3ab7c7bd73d5
Santiago Varela <santiago.varela@hotmail.com>
2023-11-02 09:13:04 -0300

    Creacion del Bottom Navigation View

--- app/src/main/java/com/example/proyecto_tp3_kotlin/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_tp3_kotlin/ui/views/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.proyecto_tp3_kotlin.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.proyecto_tp3_kotlin.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomNavBar()
+    }
+
+    private fun setupBottomNavBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.frame_container) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(navController)
     }
 }
