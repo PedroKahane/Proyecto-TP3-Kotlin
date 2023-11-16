36fbf654f1e2a1313fa488aa8d3ee07492881b7c
Santiago Varela <santiago.varela@hotmail.com>
2023-11-16 19:18:05 -0300

    mejora recyclerview

--- app/src/main/java/com/example/proyecto_tp3_kotlin/model/AdaptadorPerro.kt
+++ app/src/main/java/com/example/proyecto_tp3_kotlin/model/AdaptadorPerro.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyecto_tp3_kotlin.R
 
@@ -16,6 +17,11 @@ class AdaptadorPerro(
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val brandNombre = itemView.findViewById(R.id.brandNombre) as TextView
         val brandImagen = itemView.findViewById(R.id.brandImagen) as ImageView
+        val brandEdad = itemView.findViewById(R.id.brandEdad) as TextView
+        val brandRaza = itemView.findViewById(R.id.brandRaza) as TextView
+        val brandSubRaza = itemView.findViewById(R.id.brandSubRaza) as TextView
+        val brandGenero = itemView.findViewById(R.id.brandGenero) as TextView
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -26,6 +32,18 @@ class AdaptadorPerro(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val perro = listaPerro[position]
         holder.brandNombre.text = perro.name
+        holder.brandEdad.text = perro.age.toString()
+        holder.brandRaza.text = perro.breed
+
+        if (perro.subBreed.isEmpty()){
+            holder.brandSubRaza.text = "Sin SubRaza"
+        }else{
+            holder.brandSubRaza.text = perro.subBreed
+        }
+
+        holder.brandGenero.text = perro.gender
+        val imagenDrawable = ContextCompat.getDrawable(holder.itemView.context, R.drawable.imagen_login_perro)
+        holder.brandImagen.setImageDrawable(imagenDrawable)
 
     }
 
